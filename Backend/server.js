f4831785d151bdb306c10f7d86e2165b09629a54
Ayushi <ayushiaashish865@gmail.com>
2025-07-17 23:47:59 -0700

    changes done

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+import connectDB from './config/db.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors';
+import products from './data/products.js'; 
+
+dotenv.config();
+connectDB(); 
+ 
+const app=express();
+app.get('/',(req,res)=>{
+res.send('API is running...');
+});
+app.get('/api/products',(req,res)=>{
+res.json(products);
+});
+app.get('/api/products/:id',(req,res)=>{
+  const product=products.find(p=>p._id===req.params.id);
+  if(product){
+    res.json(product);
+  }else{
+    res.status(404).json({message:'Product not found'});
+  }
+});
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,console.log(`Server is running on ${process.env.NODE_ENVIRONMENT} port ${PORT}`.yellow.bold));
